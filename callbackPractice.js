68be4c3815474cd9927bc7e3f2fb822ebdb8b36b
Dan Seethaler <dan@seethalers.com>
2015-07-15 20:20:14 -0600

    Rewrote the remove duplicate function.

--- callbackPractice.js
+++ callbackPractice.js
@@ -24,15 +24,15 @@ and what you should write is the favNum function that makes the code above work,
 
 
 
-  //Code Here for first
+//Code Here for first
 
 var first = function (names, cb) {
-    cb(names[0]);
+	cb(names[0]);
 }
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-first(names, function(firstName){
-  console.log('The first name in names is ', firstName)
+first(names, function (firstName) {
+	console.log('The first name in names is ', firstName)
 });
 
 
@@ -42,15 +42,16 @@ first(names, function(firstName){
 
 
 
-  //Code Here for last
+//Code Here for last
 
 function last(names, cb) {
-    cb(names[names.length-1])
+    // 
+	cb(names[names.length - 1])
 }
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-last(names, function(lastName){
-  console.log('The last name in names is ', lastName);
+last(names, function (lastName) {
+	console.log('The last name in names is ', lastName);
 });
 
 
@@ -64,14 +65,14 @@ last(names, function(lastName){
 
 
 
-  //Code Here for multiply
+//Code Here for multiply
 
 function multiply(num1, num2, cb) {
-    cb(num1 + num2)
+	cb(num1 + num2)
 }
 
-multiply(4, 3, function(answer){
-  console.log('The answer is ', answer); //should console.log 12
+multiply(4, 3, function (answer) {
+	console.log('The answer is ', answer); //should console.log 12
 })
 
 
@@ -84,23 +85,23 @@ multiply(4, 3, function(answer){
 
 
 
-  //Code Here for contains
+//Code Here for contains
 
 function contains(names, name, cb) {
-    if (names.indexOf(name) !== -1) {
-        cb(true)
-    }else {
-        cb(false)
-    }
+	if (names.indexOf(name) !== -1) {
+		cb(true)
+	} else {
+		cb(false)
+	}
 }
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-contains(names, 'Colt', function(result){
-  if(result === true){
-    console.log('Colt is in the array');
-  } else {
-    console.log('Colt is not in the array');
-  }
+contains(names, 'Colt', function (result) {
+	if (result === true) {
+		console.log('Colt is in the array');
+	} else {
+		console.log('Colt is not in the array');
+	}
 });
 
 
@@ -112,74 +113,92 @@ contains(names, 'Colt', function(result){
 
 
 
-    //Code Here for uniq
+//Code Here for uniq
 
 function uniq(names, cb) {
-    names.forEach(removeDuplicates)
-    cb(names);
+
+    // Iterate backwards through names array
+	for (var i = names.length - 1; i >= 0; i--) {
+
+        // Set dupPos to the index of the first occurance of the
+        // name we're currently on
+		var dupPos = names.indexOf(names[i]);
+
+        // If the dupPos is less than the i position
+        // then there are two names
+		if (dupPos !== i) {
+            // So remove the current name
+			names.splice(i, 1)
+		}
+	}
+
+    // run the callback function with the new array
+	cb(names);
 }
 
-function removeDuplicates(currentValue, index, array) {
-    var dupPos = array.indexOf(currentValue, index + 1);
-    if (dupPos !== -1) {
-        array.splice(dupPos, 1)
-    }
-}
-
-var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-uniq(names, function(uniqArr){
-  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan', 'Dan', 'Dan', 'Dan', 'Dan', 'Dan', 'Dan', 'Jake', 'Jake', 'Jake'];
+uniq(names, function (uniqArr) {
+	console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 
 
 
 
 
-/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+	/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
 
 
 
 
-    //Code Here for each
+	//Code Here for each
 
-var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
-});
+	function each(names, cb) {
+		names.forEach(function (item, i, array) {
+			cb(item, i)
+		})
+	}
 
-
-
-
-
-/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
+	var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+	each(names, function (item, indice) {
+		console.log('The item in the ' + indice + ' position is ' + item)
+	});
 
 
 
- //code here for getUserById
 
-var users = [
-  {
-    id: '12d',
-    email: 'tyler@gmail.com',
-    name: 'Tyler',
-    address: '167 East 500 North'
-  },
-  {
-    id: '15a',
-    email: 'cahlan@gmail.com',
-    name: 'Cahlan',
-    address: '135 East 320 North'
-  },
-  {
-    id: '16t',
-    email: 'ryan@gmail.com',
-    name: 'Ryan',
-    address: '192 East 32 North'
-  },
-];
 
-getUserById(users, '16t', function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
-});
+	/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+	//code here for getUserById
+	function getUserById(users, string, cb) {
+		for (var i = 0; i < users.length; i++) {
+			if (users[i].id === string) {
+				cb(users[i]);
+			}
+		}
+	}
+
+	var users = [{
+		id: '12d',
+		email: 'tyler@gmail.com',
+		name: 'Tyler',
+		address: '167 East 500 North'
+	}, {
+		id: '15a',
+		email: 'cahlan@gmail.com',
+		name: 'Cahlan',
+		address: '135 East 320 North'
+	}, {
+		id: '16t',
+		email: 'ryan@gmail.com',
+		name: 'Ryan',
+		address: '192 East 32 North'
+	}, ];
+
+	getUserById(users, '16t', function (user) {
+		console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
+	});
